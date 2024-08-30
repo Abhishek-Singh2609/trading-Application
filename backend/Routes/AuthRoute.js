b5b2004b768f2f9a58bc0aff7642d716dffb2219
Abhishek-Singh2609 <techabhisheksingh2609@gmail.com>
2024-08-30 17:57:44 +0530

    add middlewares and AuthRoutes

--- backend/Routes/AuthRoute.js
+++ backend/Routes/AuthRoute.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { Signup,Login } = require("../Controllers/AuthController");
+const { userVerification } = require("../Middlewares/AuthMiddleware");
 
 const router = express.Router();
 
 router.post("/signup", Signup);
 router.post('/login', Login)
+router.post('/',userVerification)
 
 // router.get("/test-auth", (req, res) => {
 //     res.send("Auth Route is working!");
